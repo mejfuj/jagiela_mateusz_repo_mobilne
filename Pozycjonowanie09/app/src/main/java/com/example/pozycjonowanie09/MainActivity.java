d9a4a2d026dad4647c6aa1e8a010a2b3bea83491
mejfuj <jagielamati@gmail.com>
2022-02-01 15:57:09 +0100

    123

--- Pozycjonowanie09/app/src/main/java/com/example/pozycjonowanie09/MainActivity.java
+++ Pozycjonowanie09/app/src/main/java/com/example/pozycjonowanie09/MainActivity.java
@@ -11,4 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    File file = new File(context.getFilesDir(), filename);
+
+    String filename = "myfile";
+    String fileContents = "Hello world!";
+    try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
+        fos.write(fileContents.toByteArray());
+    }
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
 }
