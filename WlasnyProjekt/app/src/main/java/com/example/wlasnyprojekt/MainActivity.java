e0feb851bc68034f935b8bbbf6aacfbc237f6e6d
mejfuj <jagielamati@gmail.com>
2022-02-28 17:09:27 +0100

    komit

--- WlasnyProjekt/app/src/main/java/com/example/wlasnyprojekt/MainActivity.java
+++ WlasnyProjekt/app/src/main/java/com/example/wlasnyprojekt/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.wlasnyprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://"+getPackageName() + "/" + R.raw.video;
+        Uri uri = uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
